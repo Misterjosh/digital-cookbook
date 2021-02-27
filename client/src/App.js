68bd86788fa3edfeefebffff65d0578340ee97db
Josh Brooks <defaultjosh@hotmail.com>
2021-02-27 17:21:06 -0500

    Added pages using react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
-import './styling/App.css';
+import React from 'react';
+import './styling/app.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Signup from './pages/signup';
+import User from './pages/user';
+import Recipe from './pages/recipe';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Home} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/user" component={User} />
+        <Route exact path="/recipe" component={Recipe} />
+      </div>
+    </Router>
   );
 }
 
