9b6f11bddb2276e4b4096ddf095ebd955bb66a49
Josh Brooks <defaultjosh@hotmail.com>
2021-02-27 17:19:33 -0500

    Basic set up for pages

---
+++ client/src/pages/user.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../components/navbar/navbar.js';
+import Footer from '../components/footer/footer';
+
+export default function user() {
+    return (
+        <div>
+            <Navbar />
+            <Footer />
+        </div>
+    )
+}
