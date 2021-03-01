ae0c82bd93d4ae394b61a182df8aaea55bd9b3e5
Josh Brooks <defaultjosh@hotmail.com>
2021-02-28 21:45:30 -0500

    Created login form

---
+++ client/src/components/loginForm/loginForm.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './loginForm.css';
+
+export default function LogInForm({ email, password, onSubmit, handleInputChange }) {
+    return (
+        <div>
+            <form>
+                <input
+                    value={email}
+                    name="email"
+                    onChange={handleInputChange}
+                    type="email"
+                    placeholder="Email" />
+                <br />
+                <input
+                    value={password}
+                    name="password"
+                    onChange={handleInputChange}
+                    type="password"
+                    placeholder="Password" />
+                <br />
+                <button type="submit" onClick={onSubmit}>Log in</button>
+            </form>
+        </div>
+    )
+}
+
