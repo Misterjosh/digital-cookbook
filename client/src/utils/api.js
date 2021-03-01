e52bd76861047db3f3684cc002731cb3977c078e
Josh Brooks <defaultjosh@hotmail.com>
2021-02-28 21:44:43 -0500

    Added API connection

---
+++ client/src/utils/api.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+
+const API = {
+    // user
+    createUser: (userData) => {
+        return axios.post("/api/user", userData)
+    },
+
+    getUserInfo: (userId) => {
+        return axios.get("api/user/:id", userId)
+    },
+
+    getAllUsers: () => {
+        return axios.get("api/user")
+    },
+
+    updateUser: (userData) => {
+        return axios.put("api/user/:id", userData)
+    },
+
+    deleteUser: (userId) => {
+        return axios.delete("api/user/:id", userId)
+    },
+
+    // recipe
+    createRecipe: (recipeData) => {
+        return axios.post("api/recipe", recipeData)
+    },
+
+    getRecipeInfo: (recipeId) => {
+        return axios.get("api/recipe/:id", recipeId)
+    },
+
+    getAllRecipes: () => {
+        return axios.get("api/recipe")
+    },
+
+    updateRecipe: (recipeData) => {
+        return axios.put("api/recipe/:id", recipeData)
+    },
+
+    deleteRecipe: (recipeId) => {
+        return axios.delete("api/recipe/:id", recipeId)
+    }
+
+}
+
+export default API;
