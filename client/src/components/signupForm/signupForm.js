f50182a62929031e723f50480be18ec8d201ee87
Josh Brooks <defaultjosh@hotmail.com>
2021-03-01 13:32:08 -0500

    Signup Form Component

---
+++ client/src/components/signupForm/signupForm.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './signupForm.css';
+
+export default function SignUpForm({ fName, lName, email, password, onSubmit, handleInputChange }) {
+    return (
+        <div>
+            <form>
+                    <input
+                        value={fName}
+                        name="fName" 
+                        onChange={handleInputChange}
+                        type="text" 
+                        placeholder="First Name" />
+                        <br />
+                    <input
+                        value={lName}
+                        name="lName" 
+                        onChange={handleInputChange}
+                        type="text" 
+                        placeholder="Last Name" />
+                        <br />
+                    <input
+                        value={email}
+                        name="email"
+                        onChange={handleInputChange}
+                        type="email" 
+                        placeholder="Email" />
+                        <br />
+                    <input
+                        value={password}
+                        name="password"
+                        onChange={handleInputChange} 
+                        type="password" 
+                        placeholder="Password" />
+                        <br />
+                    <button type="submit" onClick={onSubmit}>Submit</button>
+                </form>
+        </div>
+    )
+}
