b6c647dc3af5d67397ad9fa068c84c9e8224dc52
Josh Brooks <defaultjosh@hotmail.com>
2021-02-28 21:26:14 -0500

    Added routes access from front end

---
+++ client/src/app/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './app.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from '../pages/home';
+import Signup from '../pages/signup';
+import User from '../pages/user';
+import Recipe from '../pages/recipe';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Home} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/user" component={User} />
+        <Route exact path="/recipe" component={Recipe} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
