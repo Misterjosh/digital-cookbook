cc070b3b661fd8edaebd531abb3657238377b2ad
Josh Brooks <defaultjosh@hotmail.com>
2021-03-01 19:12:29 -0500

    Added in new pages

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -3,8 +3,16 @@ import './app.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from '../pages/home';
 import Signup from '../pages/signup';
-import User from '../pages/user';
 import Recipe from '../pages/recipe';
+import RecipeCreate from '../pages/recipeCreate';
+import RecipeView from '../pages/recipeView';
+import RecipeViewAll from '../pages/recipesView';
+import RecipesView from '../pages/recipesViewAll';
+import RecipeDelete from '../pages/recipeDelete';
+import User from '../pages/user';
+import UserView from '../pages/userView';
+import UserEdit from '../pages/userEdit';
+import UserDelete from '../pages/userDelete';
 
 function App() {
   return (
@@ -12,8 +20,16 @@ function App() {
       <div className="App">
         <Route exact path="/" component={Home} />
         <Route exact path="/signup" component={Signup} />
-        <Route exact path="/user" component={User} />
         <Route exact path="/recipe" component={Recipe} />
+        <Route exact path="/recipe/create" component={RecipeCreate} />
+        <Route exact path="/recipe/view" component={RecipeView} />
+        <Route exact path="/user/recipes/view" component={RecipeViewAll} />
+        <Route exact path="/recipes/view" component={RecipesView} />
+        <Route exact path="/recipe/delete" component={RecipeDelete} />
+        <Route exact path="/user" component={User} />
+        <Route exact path="/user/view" component={UserView} />
+        <Route exact path="/user/edit" component={UserEdit} />
+        <Route exact path="/user/delete" component={UserDelete} />
       </div>
     </Router>
   );
