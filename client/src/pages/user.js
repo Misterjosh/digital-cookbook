878c2ba70ddc476ddb78464e6842c1a88f9d3bf6
Josh Brooks <defaultjosh@hotmail.com>
2021-04-23 22:42:43 -0400

    Adjusted for Component renaming and changing to dcb-jwt

--- client/src/pages/user.js
+++ client/src/pages/user.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from '../components/navbar/Navbar.js';
+import NavbarComp from '../components/navbar/Navbar';
 import Options from '../components/userPageMenu/userPageMenu';
 import Footer from '../components/footer/Footer';
 import jwt_decode from 'jwt-decode';
@@ -7,7 +7,7 @@ import jwt_decode from 'jwt-decode';
 let userTokenName;
 
 const getTokenName = () => {
-    userTokenName = window.localStorage.getItem('jwt')
+    userTokenName = window.localStorage.getItem('dcb-jwt')
     userTokenName = userTokenName.replace(/Bearer token: /, '')
     const decoded = jwt_decode(userTokenName);
     userTokenName = `${decoded.fName} ${decoded.lName}`
@@ -19,11 +19,11 @@ const goHome = () => {
 };
 
 export default function user() {
-    if (localStorage.getItem('jwt')) {
+    if (localStorage.getItem('dcb-jwt')) {
         getTokenName();
         return (
             <div style={{textAlign:"center"}}>
-                <Navbar />
+                <NavbarComp />
                 <h1><span className="red-span">Hello {userTokenName}!</span></h1>
                 <Options />
                 <Footer />
@@ -32,7 +32,7 @@ export default function user() {
     } else
         return (
             <div style={{textAlign:"center"}}>
-                <Navbar />
+                <NavbarComp />
                 <h1><span className="red-span">You must log in</span></h1>
                 <button onClick={goHome}>Home</button>
                 <Footer />
