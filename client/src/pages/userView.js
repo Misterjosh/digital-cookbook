9dd30fa572f503f41c32e649fb4c493015df09b0
Josh Brooks <defaultjosh@hotmail.com>
2021-05-28 03:29:52 -0400

    Finally worked out state issue. Local storage only needs id

--- client/src/pages/userView.js
+++ client/src/pages/userView.js
@@ -5,72 +5,58 @@ import jwt_decode from 'jwt-decode';
 import UserView from '../components/userView/UserView';
 import API from '../utils/api';
 
-let userTokenName;
-let userId;
-var fName = window.localStorage.getItem('first');
-var lName = window.localStorage.getItem('last');
-var email = window.localStorage.getItem('email');
-var created = window.localStorage.getItem('created');
-var updated = window.localStorage.getItem('updated');
+export default class userView extends React.Component {
+    state = {
+        loading: true,
+        person: null
+    };
 
+    async componentDidMount() {
+        if (localStorage.getItem('dcb-jwt')) {
+            const token = window.localStorage.getItem('dcb-jwt');
+            const noBearer = token.replace(/Bearer token: /, '');
+            const decoded = jwt_decode(noBearer);
+            const userId = `${decoded.id}`;
+            await API.getUserInfo({ params: { id: userId } })
+                .then((user) => {
+                    let userData = {
+                        fName: user.data.name.first,
+                        lName: user.data.name.last,
+                        email: user.data.email,
+                        created: user.data.created,
+                        updated: user.data.updated
+                    };
+            this.setState({ person: userData, loading: false });
+        })
+            .catch((error) => console.log(error));
+        }
+        
+    }
 
-const getTokenName = () => {
-    userTokenName = window.localStorage.getItem('dcb-jwt')
-    userTokenName = userTokenName.replace(/Bearer token: /, '')
-    const decoded = jwt_decode(userTokenName);
-    userTokenName = `${decoded.fName} ${decoded.lName}`
-    return userTokenName
-};
-
-const getUserId = () => {
-    userId = window.localStorage.getItem('dcb-jwt')
-    userId = userId.replace(/Bearer token: /, '')
-    const decoded = jwt_decode(userId);
-    userId = `${decoded.id}`;
-    return userId
-};
-
-const getUser = (userId) => {
-    API.getUserInfo(userId)
-    .then((user) => {
-        window.localStorage.setItem('first', user.data.name.first);
-        window.localStorage.setItem('last', user.data.name.last);
-        window.localStorage.setItem('email', user.data.email);
-        window.localStorage.setItem('created', user.data.created);
-        window.localStorage.setItem('updated', user.data.updated);
-    })
-    .catch((error) => console.log(error));
-};
-
-const goHome = () => {
-    window.location.replace("/")
-};
-
-if (localStorage.getItem('dcb-jwt')) {
-    getTokenName();
-    getUserId();
-    getUser();
+    render() {
+        const goHome = () => {
+            window.location.replace("/")
+        };
+        if (localStorage.getItem('dcb-jwt')) {
+            return (
+                <div>
+                    {this.state.loading || !this.state.person ? (
+                    <div> loading ...</div>
+                    ) : (
+                        <div>
+                            <div style={{textAlign:"center"}}>
+                                <NavbarComp />
+                                <h1><span className="red-span">Profile Details for {this.state.person.fName} {this.state.person.lName}</span></h1><br />
+                                <UserView first={this.state.person.fName} last={this.state.person.lName} email={this.state.person.email} created={this.state.person.created} updated={this.state.person.updated} />
+                                <Footer />
+                            </div>
+                        </div>
+                    )}
+                </div>
+            )
+        } else {
+            goHome();
+        }
+        
+    }
 }
-
-function userView() {
-    if (localStorage.getItem('dcb-jwt')) {
-        return (
-            <div style={{textAlign:"center"}}>
-                <NavbarComp />
-                <h1><span className="red-span">Profile Details for {userTokenName} {userId}</span></h1><br />
-                <UserView first={fName} last={lName} email={email} created={created} updated={updated} />
-                <Footer />
-            </div>
-        )
-    } else
-        return (
-            <div style={{textAlign:"center"}}>
-                <NavbarComp />
-                <h1><span className="red-span">You must log in</span></h1>
-                <button onClick={goHome}>Home</button>
-                <Footer />
-            </div>
-        )
-}
-
-export default userView;
