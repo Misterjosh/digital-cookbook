36056b58b6787fbefe89276129667a2358d4d890
Josh Brooks <defaultjosh@hotmail.com>
2021-06-02 00:34:15 -0400

    Built out functionality. After delete token removed from local storage and user is sent to home page

--- client/src/pages/userDelete.js
+++ client/src/pages/userDelete.js
@@ -1,15 +1,59 @@
 import React from 'react';
 import NavbarComp from '../components/navbar/Navbar';
 import Footer from '../components/footer/Footer';
+import API from '../utils/api';
+import jwt_decode from 'jwt-decode';
 
-function userDelete() {
-    return (
-        <div>
-            <NavbarComp />
-                <h1>Profile Deleted!</h1>
-            <Footer />
-        </div>
-    )
+export default class userDelete extends React.Component {
+    state = {
+        message: "",
+        currentId: null
+    };
+
+    async componentDidMount() {
+        if (localStorage.getItem('dcb-jwt')) {
+            const token = window.localStorage.getItem('dcb-jwt');
+            const noBearer = token.replace(/Bearer token: /, '');
+            const decoded = jwt_decode(noBearer);
+            const userId = `${decoded.id}`;
+            this.setState({ currentId: userId });
+        }
+        
+    }
+
+    render() {
+        const goHome = () => {
+            window.location.replace("/")
+        };
+        const delUser = () => {
+            if (this.state.message === "") {
+                this.setState({ message: "Warning! There is no going back. Click button again to delete your profile."})
+            } else {
+                API.deleteUser( this.state.currentId )
+                .then(() => {                    
+                this.setState({ message: "" });
+                localStorage.removeItem('dcb-jwt');
+                window.location.replace("/");
+                })
+            }
+        };
+        if (localStorage.getItem('dcb-jwt')) {
+            return (
+                <div>
+                    <div>
+                        <div style={{textAlign:"center"}}>
+                            <NavbarComp />
+                            <h1><span className="red-span">Profile Details for</span></h1><br />
+                            <div><h1><span className="red-span">{this.state.message}</span></h1></div>
+                            <button onClick={delUser}>Delete Profile</button>
+                            <Footer />
+                        </div>
+                    </div>
+                    )
+                </div>
+            )
+        } else {
+            goHome();
+        }
+    }
 }
-
-export default userDelete;
