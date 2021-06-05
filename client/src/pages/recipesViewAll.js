104c8448ae552bc728685d67c6a5cc75050e1279
Josh Brooks <defaultjosh@hotmail.com>
2021-06-05 02:28:53 -0400

    Populates from all recipes and saves selected recipe Id to local storage for /recipe to make use of

--- client/src/pages/recipesViewAll.js
+++ client/src/pages/recipesViewAll.js
@@ -1,16 +1,44 @@
 import React from 'react';
 import NavbarComp from '../components/navbar/Navbar';
 import Footer from '../components/footer/Footer';
+import API from '../utils/api';
+import RecipeButtons from '../components/lists/RecipeButtons';
 
-function recipesViewAll() {
-    return (
-        <div>
-            <NavbarComp />
-                <h1>All Recipes in the Database</h1>
-                <h2>No delete option from here</h2>
-            <Footer />
-        </div>
-    )
+export default class recipesViewAll extends React.Component {
+    state = {
+        recipes: []
+    };
+
+    async componentDidMount() {
+        await API.getAllRecipes()
+            .then((allRecipes) => {
+                this.setState({ recipes: allRecipes.data });
+            })
+            .catch((error) => console.log(error));
+    }
+
+    stickAndMove = (recId) => {
+        window.localStorage.setItem("current-recipe", recId);
+        window.location.replace("/recipe");
+    }
+
+    render() {
+        const goHome = () => {
+            window.location.replace("/")
+        };
+        
+        if (localStorage.getItem('dcb-jwt')) {
+            return (
+                <div style={{textAlign:"center"}}>
+                    <NavbarComp />
+                    <h1><span className="red-span">Choose any Recipe to view</span></h1>
+                    <RecipeButtons listVal={this.state.recipes} click={this.stickAndMove}/>
+                    <Footer />
+                </div>
+            )
+        } else {
+            goHome();
+        }
+        
+    }
 }
-
-export default recipesViewAll;
