0977b6acbe68a7a0c9d531180680056e82a7171d
Josh Brooks <defaultjosh@hotmail.com>
2021-06-05 02:35:33 -0400

    Got working properly after adding turnary operator to ArrayToUl component

---
+++ client/src/components/recipeCard/RecipeCard.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import ArrayToUl from '../../components/lists/ArrayToUl';
+
+export default class RecipeCard extends Component {
+    render() {
+        let info = this.props.recipe;
+        return (
+            <div className="row">
+                
+                <div className="col-3"></div>
+                <div className="col-6" style={{backgroundColor:'white', flexDirection: 'row', marginTop: '5rem', marginBottom: '2rem'}}>
+                    <h1 style={{marginTop: "1rem"}}><span>{info.name}</span></h1><br />
+                    <h3><span>Submitted by {info.author} on {info.submitted}</span></h3><br />
+                    <h4><span style={{flex: 1, flexwrap: 'wrap'}}>From: {info.source}</span></h4><br />
+                    <h4><span>This recipe {info.servings}</span></h4><br />
+                    <h4><span>Ingredients: </span><br />
+                        <span><ArrayToUl listVal={info.ingredients} /></span></h4><br />
+                    <h4><span>Instructions: </span><br />
+                        <span><ArrayToUl listVal={info.instructions} /></span></h4><br />
+                </div>
+                <div className="col-3"></div>
+                
+            </div>
+        )
+    }
+}
+
+
+
