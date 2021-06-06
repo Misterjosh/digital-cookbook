086827a41efa2828f0b689b9d815bf541c332079
Josh Brooks <defaultjosh@hotmail.com>
2021-06-06 14:58:52 -0400

    Fixed user always being an empty object

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -4,14 +4,13 @@ import SignupForm from '../components/signupForm/signupForm';
 import Footer from '../components/footer/Footer';
 import API from '../utils/api';
 
-// Uses state to make the forms work right
 class Signup extends Component {
     state = {
         fName: "",
         lName: "",
         email: "",
         password: "",
-        userData: {}
+        message: ""
     };
 
     // This deals with each change from a value on the form
@@ -24,10 +23,14 @@ class Signup extends Component {
 
     onSubmit = event => {
         event.preventDefault();
+        // If the user has an empty field, tell them and do nothing
         if (this.state.fName === "" || this.state.lName === "" || this.state.email === "" || this.state.password === "") {
+            this.setState({ message: "All inputs need a value!"});
             return;
         }
-        this.userData = {
+        // If the fields all have a value, make sure the message is cleared, create a user from the inputs, and make an api call
+        this.setState({ message: ""});
+        const newUser = {
             name: {
                 first: this.state.fName,
                 last: this.state.lName
@@ -36,7 +39,7 @@ class Signup extends Component {
             password: this.state.password
         }
         
-        API.createUser(this.userData)
+        API.createUser(newUser)
             .then(window.location.replace("/"))
             .catch((err) => console.log(err))
 
@@ -49,6 +52,7 @@ class Signup extends Component {
                     <div>
                         <h1><span className="red-span">Create Your Profile Here</span></h1>
                         <h3><span className="red-span">or </span><a className="blue-link" href="/">Login</a></h3>
+                        <div>{this.state.message}</div>
                             <SignupForm 
                                     fName={this.state.fName}
                                     lName={this.state.lName}
