6c0a82b53108c75345918acaed02976d38cbc502
Josh Brooks <defaultjosh@hotmail.com>
2021-06-09 00:36:18 -0400

    Removed submitted date to improve appearance

--- client/src/components/recipeCard/RecipeCard.js
+++ client/src/components/recipeCard/RecipeCard.js
@@ -10,7 +10,7 @@ export default class RecipeCard extends Component {
                 <div className="col-3"></div>
                 <div className="col-6" style={{backgroundColor:'white', flexDirection: 'row', marginTop: '5rem', marginBottom: '2rem'}}>
                     <h1 style={{marginTop: "1rem"}}><span>{info.name}</span></h1><br />
-                    <h3><span>Submitted by {info.author} on {info.submitted}</span></h3><br />
+                    <h3><span>Submitted by {info.author}</span></h3><br />
                     <h4><span style={{flex: 1, flexwrap: 'wrap'}}>From: {info.source}</span></h4><br />
                     <h4><span>This recipe {info.servings}</span></h4><br />
                     <h4><span>Ingredients: </span><br />
