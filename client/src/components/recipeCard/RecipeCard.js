0f5f6440247362d4b90450ca9e315e9b8acef05f
Josh Brooks <defaultjosh@hotmail.com>
2021-06-10 00:03:35 -0400

    Changed text for line with servings to reflect create recipe example

--- client/src/components/recipeCard/RecipeCard.js
+++ client/src/components/recipeCard/RecipeCard.js
@@ -12,7 +12,7 @@ export default class RecipeCard extends Component {
                     <h1 style={{marginTop: "1rem"}}><span>{info.name}</span></h1><br />
                     <h3><span>Submitted by {info.author}</span></h3><br />
                     <h4><span style={{flex: 1, flexwrap: 'wrap'}}>From: {info.source}</span></h4><br />
-                    <h4><span>This recipe {info.servings}</span></h4><br />
+                    <h4><span>This makes {info.servings} servings</span></h4><br />
                     <h4><span>Ingredients: </span><br />
                         <span><ArrayToUl listVal={info.ingredients} /></span></h4><br />
                     <h4><span>Instructions: </span><br />
