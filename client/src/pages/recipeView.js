f8c92f03995cd241012767ebde106fd9abfa1afa
Josh Brooks <defaultjosh@hotmail.com>
2021-06-09 23:24:06 -0400

    Page displays the user's recipes. Edit and Delete buttons added, but need function still

--- client/src/pages/recipeView.js
+++ client/src/pages/recipeView.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import NavbarComp from '../components/navbar/Navbar';
 import Footer from '../components/footer/Footer';
+import API from '../utils/api';
+import RecipeCard from '../components/recipeCard/RecipeCard';
 
-function recipeView() {
-    return (
-        <div>
-            <NavbarComp />
-                <h1>Looking at a Recipe</h1>
-            <Footer />
-        </div>
-    )
+export default class recipeView extends Component {
+    state = {
+        recipe: {}
+    };
+
+    async componentDidMount() {
+        const recipeId = localStorage.getItem("current-recipe");
+        await API.getRecipeInfo(recipeId)
+            .then((oneRecipe) => {
+                this.setState({ recipe: oneRecipe.data });
+                console.log(this.state.recipe);
+            })
+            .catch((error) => console.log(error));
+    }
+    render() {
+        const goHome = () => {
+            window.location.replace("/")
+        };
+        if (localStorage.getItem('dcb-jwt') ) {
+            return (
+                <div>
+                    <NavbarComp />
+                    <RecipeCard recipe={this.state.recipe} />
+                    <div className="row">  
+                        <div className="col-3"></div>
+                        <div className="col-6" style={{textAlign:"center"}}>
+                            <div className="row">
+                                <div className="col-2">
+                                    <button className="btn submit-btn btn-warning">Edit Recipe</button>
+                                </div>
+                                <div className="col-3">
+                                    <button className="btn submit-btn btn-danger">Delete Recipe</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-3"></div>
+                        
+                    </div>
+                    <Footer />
+                </div>
+            )
+        } else {
+            goHome();
+        }
+    }
 }
-
-export default recipeView;
