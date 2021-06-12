cfbce5f5e7ce9355be33a509391410d847a869b3
Josh Brooks <defaultjosh@hotmail.com>
2021-06-12 01:33:44 -0400

    Added padding to card sections needing it. Fixed small view website text spilling out of field with wordWrap: 'break-word'.

--- client/src/components/recipeCard/RecipeCard.js
+++ client/src/components/recipeCard/RecipeCard.js
@@ -7,18 +7,18 @@ export default class RecipeCard extends Component {
         return (
             <div className="row">
                 
-                <div className="col-3"></div>
-                <div className="col-6" style={{backgroundColor:'white', flexDirection: 'row', marginBottom: '2rem'}}>
-                    <h1 style={{marginTop: "1rem"}}><span>{info.name}</span></h1><br />
+                <div className="col-3 col-sm-1"></div>
+                <div className="col-6 col-sm-10" style={{backgroundColor:'white', paddingBottom: '2rem'}}>
+                    <h1 style={{paddingTop: "1rem"}}><span>{info.name}</span></h1><br />
                     <h3><span>Submitted by {info.author}</span></h3><br />
-                    <h4><span style={{flex: 1, flexwrap: 'wrap'}}>From: {info.source}</span></h4><br />
+                    <h4><span style={{wordWrap: "break-word"}}>From: {info.source}</span></h4><br />
                     <h4><span>This makes {info.servings} servings</span></h4><br />
                     <h4><span>Ingredients: </span><br />
                         <span><ArrayToUl listVal={info.ingredients} /></span></h4><br />
                     <h4><span>Instructions: </span><br />
                         <span><ArrayToUl listVal={info.instructions} /></span></h4><br />
                 </div>
-                <div className="col-3"></div>
+                <div className="col-3 col-sm-1"></div>
                 
             </div>
         )
