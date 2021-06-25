9da7ef796a643b525686ff396ade72525a81ecbe
Josh Brooks <defaultjosh@hotmail.com>
2021-06-24 23:25:16 -0400

    Added admin access and applicable drop down menu with items

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -13,6 +13,21 @@ import {
   DropdownItem
 } from 'reactstrap';
 import './navbar.css';
+import jwt_decode from 'jwt-decode';
+import API from '../../utils/api';
+
+const checkExp = () => {
+  const token = window.localStorage.getItem('dcb-jwt');
+  const noBearer = token.replace(/Bearer token: /, '');
+  const decoded = jwt_decode(noBearer);
+  if (( Date.now() >= (decoded.exp * 1000) )) {
+      console.log("token expired");
+      return false;
+  } else {
+      console.log("token valid");
+      return true;
+  }
+}
 
 const logOut = () => {
   localStorage.removeItem('dcb-jwt');
@@ -25,7 +40,83 @@ const NavbarComp = () => {
 
   const toggle = () => setIsOpen(!isOpen);
 
+  const [isAdmin, setAdmin] = useState(false);
+
   if (localStorage.getItem('dcb-jwt')) {
+      const token = window.localStorage.getItem('dcb-jwt');
+      const noBearer = token.replace(/Bearer token: /, '');
+      const decoded = jwt_decode(noBearer);
+      const userId = `${decoded.id}`;
+      API.getUserInfo({ params: { id: userId } })
+          .then((user) => {
+              let adminValid = {
+                  admin: user.data.admin
+              };
+              setAdmin(adminValid.admin)
+  })
+      .catch((error) => console.log(error));
+  }
+
+  if (localStorage.getItem('dcb-jwt') && checkExp() === true && isAdmin === true) {
+    return (
+        <div style={{position: "fixed", width: "100%", textAlign: "center"}}>
+          <Navbar className="navbar" light expand="md">
+            <NavbarBrand className="navbrand" href="/">Digial Cookbook <i className="fas fa-book-open"></i></NavbarBrand>
+            <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
+              <Nav className="mr-auto" navbar>
+                <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav caret>
+                        User Options
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                        <DropdownItem href="/user/view">
+                            View Profile
+                        </DropdownItem>
+                        <DropdownItem href="/user/edit">
+                            Edit Profile
+                        </DropdownItem>
+                        <DropdownItem href="/user/delete">
+                            Delete Profile
+                        </DropdownItem>
+                    </DropdownMenu>
+                </UncontrolledDropdown>
+                <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav caret>
+                        Recipes
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                        <DropdownItem href="/recipes/view">
+                            Browse Recipes
+                        </DropdownItem>
+                        <DropdownItem href="/recipes/view/user">
+                            Your Recipes
+                        </DropdownItem>
+                        <DropdownItem href="/recipe/create">
+                            Create a Recipe
+                        </DropdownItem>
+                    </DropdownMenu>
+                </UncontrolledDropdown>
+                <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav caret>
+                        Admin Options
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                        <DropdownItem href="/admin/users">
+                            View All Users
+                        </DropdownItem>
+                        <DropdownItem href="/admin/recipes">
+                            View All Recipes
+                        </DropdownItem>
+                    </DropdownMenu>
+                </UncontrolledDropdown>
+              </Nav>
+            <button className="btn submit-btn" style={{backgroundColor: "#c3c0c0"}} onClick={logOut}>Log Out</button>
+            </Collapse>
+          </Navbar>
+        </div>
+      );
+  } else if (localStorage.getItem('dcb-jwt') && checkExp() === true) {
     return (
         <div style={{position: "fixed", width: "100%", textAlign: "center"}}>
           <Navbar className="navbar" light expand="md">
