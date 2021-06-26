a222af2c5d3156ae80722c24cfca628877bac390
Josh Brooks <defaultjosh@hotmail.com>
2021-06-25 22:41:27 -0400

    Changed card layout, wording and buttons

--- client/src/pages/recipeCreate.js
+++ client/src/pages/recipeCreate.js
@@ -6,41 +6,41 @@ import jwt_decode from 'jwt-decode';
 
 export default class recipeCreate extends Component {
 
-    state = {
-        message: '',
-        name: '',
-        source: '',
-        servings: '',
-        instVal: '',
-        ingVal: '',
-        instList: [],
-        ingList: [],
-        author: '',
-        authorId: ''
-      };
+  state = {
+      message: '',
+      name: '',
+      source: '',
+      servings: '',
+      instVal: '',
+      ingVal: '',
+      instList: [],
+      ingList: [],
+      author: '',
+      authorId: ''
+    };
 
 // get user name and id to place in recipe object after setting their states with the values
-      async componentDidMount() {
-        if (localStorage.getItem('dcb-jwt')) {
-            const token = window.localStorage.getItem('dcb-jwt');
-            const noBearer = token.replace(/Bearer token: /, '');
-            const decoded = jwt_decode(noBearer);
-            const userId = `${decoded.id}`;
-            await API.getUserInfo({ params: { id: userId } })
-                .then((user) => {
-                    this.setState({ author:  user.data.name.first + ' ' + user.data.name.last, authorId: userId})
-        })
-            .catch((error) => console.log(error));
-        }
+    async componentDidMount() {
+      if (localStorage.getItem('dcb-jwt')) {
+          const token = window.localStorage.getItem('dcb-jwt');
+          const noBearer = token.replace(/Bearer token: /, '');
+          const decoded = jwt_decode(noBearer);
+          const userId = `${decoded.id}`;
+          await API.getUserInfo({ params: { id: userId } })
+              .then((user) => {
+                  this.setState({ author:  user.data.name.first + ' ' + user.data.name.last, authorId: userId})
+      })
+          .catch((error) => console.log(error));
+      }
     }
 
 // On changes for each input
-      handleInputChange = event => {
-        const { name, value } = event.target;
-        this.setState({
-            [name]: value
-        });
-      };
+    handleInputChange = event => {
+      const { name, value } = event.target;
+      this.setState({
+          [name]: value
+      });
+    };
 
 // Ingredients array add and delete functions
     onAddIng = () => {
@@ -117,108 +117,115 @@ export default class recipeCreate extends Component {
 
 // The rendered component
     render() {
-        const goHome = () => {
-            window.location.replace("/")
-        };
-        const checkExp = () => {
-          const token = window.localStorage.getItem('dcb-jwt');
-          const noBearer = token.replace(/Bearer token: /, '');
-          const decoded = jwt_decode(noBearer);
-          if (( Date.now() >= (decoded.exp * 1000) )) {
-              console.log("token expired");
-              return false;
-          } else {
-              console.log("token valid");
-              return true;
-          }
-        }
-        if (localStorage.getItem('dcb-jwt') && checkExp() === true) {
-            return (
-                <div>
-                    <NavbarComp />
-
-                    <div className="container" style={{paddingTop: "6rem", paddingBottom: "5rem"}}>
-
-                      <div>
-                        <h3>Recipe Name: </h3>
-                        <input type="text" name="name" value={this.state.name} onChange={this.handleInputChange} />
-                      </div>
-
-                      <div>
-                        <h3>Source: </h3>
-                        <input type="text" name="source" value={this.state.source} onChange={this.handleInputChange} />
-                      </div>
-
-                      <div>
-                        <h3>Number of Servings: </h3>
-                        <input type="text" name="servings" value={this.state.servings} onChange={this.handleInputChange} /> 
-                        <span>Shows up as: </span>
-                        <span style={{backgroundColor: "white"}}>This makes {this.state.servings} servings.</span>
-                      </div>
-
-                      <div>
-                          <h3>Ingredients: </h3>
-                          <ul>
-                              {(this.state.ingList).map((item, index) => (
-                                  <li key={item.id}>{item.value} 
-                                      <button className="btn btn-danger btn-sm" onClick={() => this.onDeleteIng(index)}>x</button>
-                                  </li>
-                              ))}
-                          </ul>
-
-                          <input 
-                          type="text"
-                          name="ingVal"
-                          value={this.state.ingVal}
-                          onChange={this.handleInputChange}
-                          />
-
-                          <button
-                          type="button"
-                          onClick={this.onAddIng}
-                          disabled={!this.state.ingVal}
-                          >
-                          Add
-                          </button>
-                      </div>
-
-                      <div>
-                          <h3>Instructions: </h3>
-                          <ul>
-                              {(this.state.instList).map((item, index) => (
-                                  <li key={item.id}>{item.value} 
-                                      <button className="btn btn-danger btn-sm" onClick={() => this.onDeleteInst(index)}>x</button>
-                                  </li>
-                              ))}
-                          </ul>
-
-                          <input 
-                          type="text"
-                          name="instVal"
-                          value={this.state.instVal}
-                          onChange={this.handleInputChange}
-                          />
-
-                          <button
-                          type="button"
-                          onClick={this.onAddInst}
-                          disabled={!this.state.instVal}
-                          >
-                          Add
-                          </button>
-                      </div>
-
-                      <div>
-                        {this.state.message}
-                      </div>
-
-                      <button onClick={this.onSubmit}>Submit Recipe</button>
-
-                    </div>
-                    <Footer />
-                </div>
-            )
+      const goHome = () => {
+          window.location.replace("/")
+      };
+      const checkExp = () => {
+        const token = window.localStorage.getItem('dcb-jwt');
+        const noBearer = token.replace(/Bearer token: /, '');
+        const decoded = jwt_decode(noBearer);
+        if (( Date.now() >= (decoded.exp * 1000) )) {
+            console.log("token expired");
+            return false;
         } else {
+            console.log("token valid");
+            return true;
+        }
+      }
+      if (localStorage.getItem('dcb-jwt') && checkExp() === true) {
+          return (
+            <div>
+                <NavbarComp />
+
+                <form className="container" style={{paddingTop: "5rem", paddingBottom: "5rem"}}>
+
+                  <h1 style={{textAlign: "center"}}><span className="red-span">Create a new Recipe</span></h1>
+
+                  <div style={{marginTop: "0", paddingTop: "0.5rem", paddingBottom: "0.5rem", backgroundColor: "white"}}>
+                    <h3>Recipe Name: </h3>
+                    <input type="text" name="name" value={this.state.name} onChange={this.handleInputChange} />
+                  </div>
+
+                  <div style={{marginTop: "0", paddingTop: "0", paddingBottom: "0.5rem", backgroundColor: "white"}}>
+                    <h3>Source: </h3>
+                    <input type="text" name="source" value={this.state.source} onChange={this.handleInputChange} />
+                  </div>
+
+                  <div style={{marginTop: "0", paddingTop: "0", paddingBottom: "0.5rem", backgroundColor: "white"}}>
+                    <h3>Number of Servings: </h3>
+                    <input type="text" name="servings" value={this.state.servings} onChange={this.handleInputChange} /> 
+                    <div className="row" style={{padding: "1rem"}}>
+                      <div className="col">
+                        <h5>Shows up as: This makes <span style={{backgroundColor: "lightblue"}}>{this.state.servings}</span> servings.</h5>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div style={{marginTop: "0", paddingTop: "0", paddingBottom: "0.5rem", backgroundColor: "white"}}>
+                      <h3>Ingredients: </h3>
+                      <ul>
+                          {(this.state.ingList).map((item, index) => (
+                              <li key={item.id}>{item.value} 
+                                  <button className="btn btn-danger btn-sm" onClick={() => this.onDeleteIng(index)}>x</button>
+                              </li>
+                          ))}
+                      </ul>
+
+                      <input 
+                      type="text"
+                      name="ingVal"
+                      value={this.state.ingVal}
+                      onChange={this.handleInputChange}
+                      />
+
+                      <button
+                      type="button"
+                      onClick={this.onAddIng}
+                      disabled={!this.state.ingVal}
+                      className="btn submit-btn btn-success"
+                      >
+                      Add
+                      </button>
+                  </div>
+
+                  <div style={{marginTop: "0", paddingTop: "0", paddingBottom: "0.5rem", backgroundColor: "white"}}>
+                      <h3>Instructions: </h3>
+                      <ul>
+                          {(this.state.instList).map((item, index) => (
+                              <li key={item.id}>{item.value} 
+                                  <button className="btn btn-danger btn-sm" onClick={() => this.onDeleteInst(index)}>x</button>
+                              </li>
+                          ))}
+                      </ul>
+
+                      <input 
+                      type="text"
+                      name="instVal"
+                      value={this.state.instVal}
+                      onChange={this.handleInputChange}
+                      />
+
+                      <button
+                      type="button"
+                      onClick={this.onAddInst}
+                      disabled={!this.state.instVal}
+                      className="btn submit-btn btn-success"
+                      >
+                      Add
+                      </button>
+                  </div>
+
+                  <div style={{marginTop: "0", paddingTop: "0.5rem", paddingBottom: "0.5rem"}}>
+                    <h1><span className="red-span">{this.state.message}</span></h1>
+                  </div>
+
+                  <button style={{paddingBottom: "0.5rem"}} onClick={this.onSubmit} className="btn submit-btn btn-primary"><i class="fas fa-plus"></i> Submit Recipe</button>
+                </form>
+
+                <Footer />
+            </div>
+            )
+      } else {
             goHome();
         }
     }
