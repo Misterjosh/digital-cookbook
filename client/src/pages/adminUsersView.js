a2ac44f4c5bca38ae4026695b2ea38e78beb12c6
Josh Brooks <defaultjosh@hotmail.com>
2021-06-26 02:05:21 -0400

    Cards now display all user info. Delete specific user works

--- client/src/pages/adminUsersView.js
+++ client/src/pages/adminUsersView.js
@@ -2,8 +2,23 @@ import React, { Component } from 'react';
 import NavbarComp from '../components/navbar/Navbar';
 import Footer from '../components/footer/Footer';
 import jwt_decode from 'jwt-decode';
+import API from '../utils/api';
+import AdminUsersDisplay from '../components/admin/AdminUsersDisplay';
 
 export default class adminUsersView extends Component {
+    state = {
+        users: []
+    };
+
+    async componentDidMount() {
+        await API.getAllUsers()
+            .then((allUsers) => {
+                this.setState({ users: allUsers.data });
+                // console.log(this.state.users);
+            })
+            .catch((error) => console.log(error));
+    }
+
     render() {
         const goHome = () => {
             window.location.replace("/")
@@ -20,11 +35,19 @@ export default class adminUsersView extends Component {
                 return true;
             }
         }
+        const onDeleteClick = (idToDelete) => {
+            API.deleteUser(idToDelete)
+            .then(() => {
+            window.location.replace("/admin/users");
+            })
+            .catch(error => console.log(error));
+    }
         if (localStorage.getItem('dcb-jwt') && checkExp() === true) {
             return (
-                <div>
+                <div style={{}}>
                     <NavbarComp />
-                    <h1 style={{paddingTop: "5rem"}}>Generic Admin page to view users.</h1>
+                    <h1 style={{paddingTop: "5rem", textAlign: "center"}}><span className="red-span">Admin Users Page</span></h1>
+                    <AdminUsersDisplay arrUsers={this.state.users} delClick={onDeleteClick} />
                     <Footer />
                 </div>
             )
