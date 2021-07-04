5f902c2b2b48ac05eaf24660feaf05c5088f5321
Josh Brooks <defaultjosh@hotmail.com>
2021-07-04 18:43:02 -0400

    Created pages for an admin to edit a user or recipe

---
+++ client/src/pages/adminUserEdit.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import NavbarComp from '../components/navbar/Navbar';
+import Footer from '../components/footer/Footer';
+import jwt_decode from 'jwt-decode';
+import API from '../utils/api';
+
+export default class adminPage extends Component {
+    render() {
+        const goHome = () => {
+            window.location.replace("/")
+        };
+        const checkExp = () => {
+            const token = window.localStorage.getItem('dcb-jwt');
+            const noBearer = token.replace(/Bearer token: /, '');
+            const decoded = jwt_decode(noBearer);
+            if (( Date.now() >= (decoded.exp * 1000) )) {
+                console.log("token expired");
+                return false;
+            } else {
+                console.log("token valid");
+                return true;
+            }
+        }
+        if (localStorage.getItem('dcb-jwt') && checkExp() === true) {
+            return (
+                <div>
+                    <NavbarComp />
+                    <h1 style={{paddingTop: "5rem"}}>Generic Admin page to edit a user.</h1>
+                    <Footer />
+                </div>
+            )
+        } else {
+            goHome();
+        }
+    }
+}
