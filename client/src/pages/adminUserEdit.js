2ae0382cf8f0e3653b9b63c089105dad1854994d
Josh Brooks <defaultjosh@hotmail.com>
2021-07-06 19:23:11 -0400

    Used loading trick to get user card to work

--- client/src/pages/adminUserEdit.js
+++ client/src/pages/adminUserEdit.js
@@ -3,8 +3,38 @@ import NavbarComp from '../components/navbar/Navbar';
 import Footer from '../components/footer/Footer';
 import jwt_decode from 'jwt-decode';
 import API from '../utils/api';
+import UserInfoCard from '../components/admin/edit/UserInfoCard';
 
 export default class adminPage extends Component {
+    state = {
+        validAdmin: true,
+        loading: true,
+        user: null
+    };
+
+    async componentDidMount() {
+        const idToEdit = window.localStorage.getItem('admin-id-user');
+        const token = window.localStorage.getItem('dcb-jwt');
+        const noBearer = token.replace(/Bearer token: /, '');
+        const decoded = jwt_decode(noBearer);
+        const userId = `${decoded.id}`;
+        await Promise.all([API.getUserInfo({ params: { id: idToEdit } }), API.getUserInfo({ params: { id: userId } })])
+            .then(([user, userInfo]) => {
+                let userData = {
+                    fName: user.data.name.first,
+                    lName: user.data.name.last,
+                    email: user.data.email,
+                    admin: `${user.data.admin}`
+                };
+                this.setState({ 
+                    user: userData, 
+                    validAdmin: userInfo.data.admin, 
+                    loading: false 
+                });
+            })
+            .catch((error) => console.log(error));
+    }
+
     render() {
         const goHome = () => {
             window.location.replace("/")
@@ -24,9 +54,21 @@ export default class adminPage extends Component {
         if (localStorage.getItem('dcb-jwt') && checkExp() === true) {
             return (
                 <div>
-                    <NavbarComp />
-                    <h1 style={{paddingTop: "5rem"}}>Generic Admin page to edit a user.</h1>
-                    <Footer />
+                    {this.state.loading || !this.state.user ? (
+                    <div> loading ...</div>
+                    ) : ( 
+                    <div>
+                        <NavbarComp />
+                        <h1 className="red-span" style={{paddingTop: "5rem", textAlign: "center"}}>Generic Admin page to edit a user.</h1>
+                        <UserInfoCard
+                            first={this.state.user.fName} 
+                            last={this.state.user.lName} 
+                            email={this.state.user.email}
+                            admin={(`${this.state.user.admin}`.toUpperCase())}
+                        />
+                        <Footer />
+                    </div>
+                    )}
                 </div>
             )
         } else {
