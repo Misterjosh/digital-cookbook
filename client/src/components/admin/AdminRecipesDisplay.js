2e723e1f252e69dc33be9590c45c206544b1f78a
Josh Brooks <defaultjosh@hotmail.com>
2021-07-21 22:35:37 -0400

    Changed class to className to clear React errors

--- client/src/components/admin/AdminRecipesDisplay.js
+++ client/src/components/admin/AdminRecipesDisplay.js
@@ -6,12 +6,12 @@ export default class AdminRecipesDisplay extends Component {
         return (
             <div className="container" style={{paddingTop: "1%", paddingBottom: "1%", margin: "0 auto", justifyContent: "center", overflow: "hidden", marginBottom: "10%"}}>
                 {recipesArray.map((recipe, click) => (
-                    <div className="child card" key={recipe._id} class="card" style={{marginTop: "1rem", display: "inline-block", marginRight: "1rem"}}>
-                        <div class="card-body">
-                            <h5 class="card-title">Recipe Name: {recipe.name}</h5>
-                            <h6 class="card-subtitle mb-2">Author: {recipe.author}</h6>
-                            <button className="btn submit-btn btn-warning" onClick={() => this.props.editClick(recipe._id)}><i class="fas fa-edit"></i> Edit</button>
-                            <button className="btn submit-btn btn-danger" onClick={() => this.props.delClick(recipe._id)}><i class="fas fa-trash-alt"></i> Delete</button>
+                    <div className="child card" key={recipe._id} style={{marginTop: "1rem", display: "inline-block", marginRight: "1rem"}}>
+                        <div className="card-body">
+                            <h5 className="card-title">Recipe Name: {recipe.name}</h5>
+                            <h6 className="card-subtitle mb-2">Author: {recipe.author}</h6>
+                            <button className="btn submit-btn btn-warning" onClick={() => this.props.editClick(recipe._id)}><i className="fas fa-edit"></i> Edit</button>
+                            <button className="btn submit-btn btn-danger" onClick={() => this.props.delClick(recipe._id)}><i className="fas fa-trash-alt"></i> Delete</button>
                         </div>
                     </div>
                 ))}
