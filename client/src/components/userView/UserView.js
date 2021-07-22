f3c6a66ed122a6ef2961daf38653e82ed2242ee2
Josh Brooks <defaultjosh@hotmail.com>
2021-07-21 22:47:25 -0400

    Fixed duplicate className prop error and class to className errors

--- client/src/components/userView/UserView.js
+++ client/src/components/userView/UserView.js
@@ -4,13 +4,13 @@ import './userView.css';
 export default function UserView({ first, last, email, created, updated, admin }) {
     return (
         <div className="container" style={{paddingTop: "1%", paddingBottom: "1%", margin: "0 auto", justifyContent: "center", overflow: "hidden", marginBottom: "10%"}}>
-            <div className="child" class="card" style={{marginTop: "1rem", zIndex: "-5", display: "inline-block"}}>
-                <div class="card-body" style={{textAlign: "left"}}>
-                    <h5 class="card-title"><span>Name: </span>{first} {last}</h5>
-                    <h6 class="card-subtitle mb-2"><span>Email: </span>{email}</h6>
-                    <h6 class="card-subtitle mb-2"><span>Created: </span>{created}</h6>
-                    <h6 class="card-subtitle mb-2"><span>Updated: </span>{updated}</h6>
-                    <h6 class="card-subtitle mb-2"><span>Admin: </span>{(`${admin}`).toUpperCase()}</h6>
+            <div className="child card" style={{marginTop: "1rem", zIndex: "-5", display: "inline-block"}}>
+                <div className="card-body" style={{textAlign: "left"}}>
+                    <h5 className="card-title"><span>Name: </span>{first} {last}</h5>
+                    <h6 className="card-subtitle mb-2"><span>Email: </span>{email}</h6>
+                    <h6 className="card-subtitle mb-2"><span>Created: </span>{created}</h6>
+                    <h6 className="card-subtitle mb-2"><span>Updated: </span>{updated}</h6>
+                    <h6 className="card-subtitle mb-2"><span>Admin: </span>{(`${admin}`).toUpperCase()}</h6>
                 </div>
             </div>
         </div>
