4b5bfeeeca864120bc9068c7fddd66f4ca90c123
Josh Brooks <defaultjosh@hotmail.com>
2021-07-21 22:37:07 -0400

    Removed duplicate className prop and changed class to className to clear React errors

--- client/src/components/admin/AdminUsersDisplay.js
+++ client/src/components/admin/AdminUsersDisplay.js
@@ -6,15 +6,15 @@ export default class adminUsersDisplay extends Component {
         return (
             <div className="container" style={{paddingTop: "1%", paddingBottom: "1%", margin: "0 auto", justifyContent: "center", overflow: "hidden", marginBottom: "10%"}}>
                 {usersArray.map((user, click) => (
-                    <div className="child" key={user._id} class="card" style={{marginTop: "1rem", display: "inline-block", marginRight: "1rem"}}>
-                        <div class="card-body">
-                            <h5 class="card-title">Name: {user.name.first} {user.name.last}</h5>
-                            <h6 class="card-subtitle mb-2">Email: {user.email}</h6>
-                            <h6 class="card-subtitle mb-2">Created: {user.created}</h6>
-                            <h6 class="card-subtitle mb-2">Updated: {user.updated}</h6>
-                            <h6 class="card-subtitle mb-2">Admin: {(`${user.admin}`).toUpperCase()}</h6>
-                            <button className="btn submit-btn btn-warning" onClick={() => this.props.editClick(user._id)}><i class="fas fa-edit"></i> Edit</button>
-                            <button className="btn submit-btn btn-danger" onClick={() => this.props.delClick(user._id)}><i class="fas fa-trash-alt"></i> Delete</button>
+                    <div className="child card" key={user._id} style={{marginTop: "1rem", display: "inline-block", marginRight: "1rem"}}>
+                        <div className="card-body">
+                            <h5 className="card-title">Name: {user.name.first} {user.name.last}</h5>
+                            <h6 className="card-subtitle mb-2">Email: {user.email}</h6>
+                            <h6 className="card-subtitle mb-2">Created: {user.created}</h6>
+                            <h6 className="card-subtitle mb-2">Updated: {user.updated}</h6>
+                            <h6 className="card-subtitle mb-2">Admin: {(`${user.admin}`).toUpperCase()}</h6>
+                            <button className="btn submit-btn btn-warning" onClick={() => this.props.editClick(user._id)}><i className="fas fa-edit"></i> Edit</button>
+                            <button className="btn submit-btn btn-danger" onClick={() => this.props.delClick(user._id)}><i className="fas fa-trash-alt"></i> Delete</button>
                         </div>
                     </div>
                 ))}
