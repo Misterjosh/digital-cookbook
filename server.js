b487613400e117b8354438c5fa50223472873065
Josh Brooks <defaultjosh@hotmail.com>
2021-08-06 05:12:54 -0400

    Minor change to code for troubleshooting.

--- server.js
+++ server.js
@@ -30,10 +30,10 @@ app.use(routes);
 
 // give server access to the react application when env is production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, '/client/build')));
+  app.use(express.static('/client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 } else {
   app.get("/", (req, res) => {
