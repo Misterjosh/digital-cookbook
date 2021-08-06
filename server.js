71ac5b92d2a56b4c3562872f5a188f90db15bffc
Josh Brooks <defaultjosh@hotmail.com>
2021-08-06 05:13:43 -0400

    Removed else statement for troubleshooting.

--- server.js
+++ server.js
@@ -35,10 +35,6 @@ if (process.env.NODE_ENV === "production") {
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
-} else {
-  app.get("/", (req, res) => {
-    res.send("Api running");
-  });
 }
 
 // Set up the app to run using PORT
